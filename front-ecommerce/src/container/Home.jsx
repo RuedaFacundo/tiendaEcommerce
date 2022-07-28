323dd30ec0bf248ce4afacba2c72bb8bd430b423
Facundo <facundoarueda@hotmail.com>
2022-07-28 18:39:15 -0300

    Card product

--- front-ecommerce/src/container/Home.jsx
+++ front-ecommerce/src/container/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CardProducto from '../components/CardProducto'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Productos destacados</h1>
+      <CardProducto></CardProducto>
     </div>
   )
 }
--- front-ecommerce/src/routes/App.jsx
+++ front-ecommerce/src/routes/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
           <Route path='/carrito' element={<Carrito />}></Route>
           <Route path='/productos' element={<Productos />}></Route>
           <Route path='' element={<Error404 />}></Route>
