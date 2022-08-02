7f5728c94b9e656c8bb49055a2c3c87340d7bced
Facundo <facundoarueda@hotmail.com>
2022-08-01 21:11:36 -0300

    Fix barra de busqueda

--- front-ecommerce/src/components/BarraDeBusqueda.jsx
+++ front-ecommerce/src/components/BarraDeBusqueda.jsx
@@ -1,25 +1,13 @@
 import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import '../Statics/css/Header.css'
 
 const BarraDeBusqueda = () => {
   return (
-    <TextField
-      label="Buscar..."
-      className='barraBusqueda'
-      InputProps={{
-        endAdornment: (
-          <InputAdornment>
-            <IconButton>
-              <SearchIcon />
-            </IconButton>
-          </InputAdornment>
-        )
-      }}
-    />
+    <div class="group">
+      <SearchIcon />
+      <input placeholder="Buscar..." type="search" class="input" />
+    </div>
   );
 }
 
--- front-ecommerce/src/components/Carrusel.jsx
+++ front-ecommerce/src/components/Carrusel.jsx
@@ -8,25 +8,25 @@ return (
 	<div style={{ display: 'block' }} className='ancho'>
 	<Carousel>
 		<Carousel.Item interval={1500}>
-		<img
-			className="d-block w-100"
-      src="https://http2.mlstatic.com/D_NQ_951840-MLA50929759526_072022-OO.webp"
-			alt="Image One"
-		/>
+      <img
+        className="d-block w-100"
+        src="https://http2.mlstatic.com/D_NQ_951840-MLA50929759526_072022-OO.webp"
+        alt="Oferta 1"
+      />
 		</Carousel.Item>
 		<Carousel.Item interval={500}>
-		<img
-			className="d-block w-100"
-      src="https://http2.mlstatic.com/D_NQ_730597-MLA50899717756_072022-OO.webp"
-			alt="Image Two"
-		/>
+      <img
+        className="d-block w-100"
+        src="https://http2.mlstatic.com/D_NQ_730597-MLA50899717756_072022-OO.webp"
+        alt="Oferta 2"
+      />
 		</Carousel.Item>
     <Carousel.Item interval={500}>
-		<img
-			className="d-block w-100"
-      src="https://http2.mlstatic.com/D_NQ_676173-MLA50899923109_072022-OO.webp"
-			alt="Image Two"
-		/>
+      <img
+        className="d-block w-100"
+        src="https://http2.mlstatic.com/D_NQ_676173-MLA50899923109_072022-OO.webp"
+        alt="Oferta 3"
+      />
 		</Carousel.Item>
 	</Carousel>
 	</div>
